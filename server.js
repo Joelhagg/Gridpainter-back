18ab0464c5d3b914e32052b269ffb4b65cc385dd
Louise Rosenqvist <kimle.rosenqvist@gmail.com>
2022-08-25 19:13:04 +0200

    moved rooms to jsonfile

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = require("http").createServer(app);
 const port = process.env.PORT || 3001;
 const socketIo = require("socket.io");
 const picturesArray = require("./assets/fields.json");
+const { readFileSync, writeFileSync } = require("fs");
 
 const io = socketIo(server, {
   cors: {
@@ -16,25 +17,34 @@ const io = socketIo(server, {
 });
 
 const fieldsRouter = require("./routes/fields");
-const indexRouter = require("./routes/index");
 
 app.use(cors());
 // app.use("/", indexRouter);
 app.use("/fields", fieldsRouter);
 app.use(bodyParser.json());
 
-// Sparade rummen
-const rooms = [];
-
 app.get("/", (req, res) => {
   res.json(picturesArray);
 });
 
+app.get('/rooms', (req, res) => {
+  const allRooms = readFileSync('./assets/rooms.json');
+  res.json(allRooms)
+})
+
 app.post("/rooms", (req, res) => {
+  let allRooms = readFileSync('./assets/rooms.json');
+
+  if(allRooms){
+    allRooms = JSON.parse(allRooms)
+    allRooms.push({room: req.body.room})
+    writeFileSync('./assets/rooms.json', JSON.stringify(allRooms))
+  }
+
   // skriv hantering för rum som inte finns
 
   // Här tas det nyskapade rummet emot och sparas
-  rooms.push(req.body.room);
+  //rooms.push(req.body.room);
 
   // Nytt rum skickas till fronten
   io.emit("roomCreated", req.body.room);
@@ -70,15 +80,15 @@ io.on("connection", function (socket) {
     io.emit("drawing", msg);
   });
 
-  socket.on("new room", function (roomName) {
+  /*socket.on("new room", function (roomName) {
     let newRoom = { roomName: roomName };
     rooms.push(newRoom);
     io.emit("new room", roomName);
-  });
+  });*/
 });
 
 server.listen(port, () => {
   console.log("listens to port " + port);
 });
 
-module.exports = app;
+module.exports = {app, io};
