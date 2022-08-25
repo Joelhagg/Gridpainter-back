5aed3da0dc0a4fe13e20da515f55dc78cd26ae38
joelhagg <joel.pettersson90@gmail.com>
2022-08-25 14:55:32 +0200

    add handling for new room

--- server.js
+++ server.js
@@ -1,57 +1,75 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const server = require('http').createServer(app)
-const port = process.env.PORT || 3001
-const socketIo = require('socket.io')
-const picturesArray = require('./assets/fields.json')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const server = require("http").createServer(app);
+const port = process.env.PORT || 3001;
+const socketIo = require("socket.io");
+const picturesArray = require("./assets/fields.json");
 
 const io = socketIo(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
-    credentials: true
-  }
-})
+    credentials: true,
+  },
+});
 
-const fieldsRouter = require('./routes/fields')
-const indexRouter = require('./routes/index')
+const fieldsRouter = require("./routes/fields");
+const indexRouter = require("./routes/index");
+const picturesArray = require("../assets/fields.json");
 
 app.use(cors());
-app.use('/', indexRouter)
-app.use('/fields', fieldsRouter)
+// app.use("/", indexRouter);
+app.use("/fields", fieldsRouter);
+
+const rooms = {};
+
+app.get("/", (req, res) => {
+  res.json(picturesArray);
+});
+
+app.post("/room", (req, res) => {
+  // skriv hantering för rum som inte finns
+
+  // Här tas det nyskapade rummet emot
+  rooms = req.body.room;
+  console.log(rooms);
+
+  // Nytt rum
+  io.emit("roomCreated", req.body.room);
+});
 
 io.on("connection", function (socket) {
   console.log("a user connected");
   io.emit("history", picturesArray);
 
   socket.on("disconnect", function () {
-      console.log("user disconnected");
+    console.log("user disconnected");
   });
 
   socket.on("chat message", function (msg) {
-      console.log(msg);
-      io.emit("chat message", msg);
+    console.log(msg);
+    io.emit("chat message", msg);
   });
 
   socket.on("drawing", function (msg) {
-      console.log(msg);
+    console.log(msg);
 
-      for (let i = 0; i < picturesArray.length; i++) {
-          const pixel = picturesArray[i];
+    for (let i = 0; i < picturesArray.length; i++) {
+      const pixel = picturesArray[i];
 
-          if (pixel.position == msg.position) {
-              pixel.color = msg.color
-          }
+      if (pixel.position == msg.position) {
+        pixel.color = msg.color;
       }
-      console.log(picturesArray);
-     
-      io.emit("drawing", msg);
+    }
+    console.log(picturesArray);
+
+    io.emit("drawing", msg);
   });
-})
+});
 
 server.listen(port, () => {
   console.log("listens to port " + port);
-})
+});
 
 module.exports = app;
