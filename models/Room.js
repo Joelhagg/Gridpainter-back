6d0bee77c45165d9bc15b7b483a0f90150f4b119
johanna-almqvist <94167705+johanna-almqvist@users.noreply.github.com>
2022-08-31 23:28:15 +0200

    added roomsArray

--- models/Room.js
+++ models/Room.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const RoomSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   id: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+  },
+  members: {
+    type: Array,
   },
   savedImgs: {
-    type: Array
-  }
-})
+    type: Array,
+  },
+});
 
-module.exports = mongoose.model('Room', RoomSchema)
+module.exports = mongoose.model("Room", RoomSchema);
